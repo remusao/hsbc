7de77ffdd7ad30103f4170009f53012506c6bc42
Rémi Berson <remi@cliqz.com>
2017-02-11 19:45:19 +0100

    Add a few comments

--- hsbc.hs
+++ hsbc.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 
@@ -7,10 +5,8 @@ import Data.Char (isDigit)
 import Data.Maybe (fromJust)
 
 
-data Assoc = Left | Right
-
 data Operator =
-    Add
+      Add
     | Sub
     | Mult
     | Div
@@ -19,10 +15,28 @@ data Operator =
 
 data Token =
       Number Integer
-    | BinOp Operator
+    | BinOp  Operator
     | LeftBracket
     | RightBracket
     deriving (Show, Eq)
+
+
+isOp :: Token -> Bool
+isOp (BinOp _) = True
+isOp _ = False
+
+
+precedence :: Operator -> Int
+precedence Add  = 2
+precedence Sub  = 2
+precedence Mult = 3
+precedence Div  = 3
+
+
+hasLowerPrecedence :: Token -> Token -> Bool
+hasLowerPrecedence (BinOp op1) (BinOp op2)
+    | precedence op1 <= precedence op2 = True
+hasLowerPrecedence _ _ = False
 
 
 tokenize :: String -> [Token]
@@ -40,43 +54,39 @@ tokenize expr@(x:xs)
     | otherwise = tokenize xs
 
 
-isOp :: Token -> Bool
-isOp (BinOp _) = True
-isOp _ = False
-
-
-precedence :: Operator -> Int
-precedence Add = 2
-precedence Sub = 2
-precedence Mult = 3
-precedence Div = 3
-
-
 -- Shunting-Yard algorithm
 toRPN :: [Token] -> [Token]
 toRPN = reverse . go [] []
     where
+        -- When there is no more token available, then pop remaining
+        -- operators from the stack. If there is a bracket remaining,
+        -- this should be a syntax error.
         go stack result [] =
             let (operators, rest) = span isOp stack
             in reverse operators ++ result
+        -- Handle number, which are pushed to the result queue directly.
         go stack result (Number i:xs) = go stack (Number i:result) xs
+        -- Opening bracket, pushed on the stack.
         go stack result (LeftBracket:xs) = go (LeftBracket:stack) result xs
+        -- Closing bracket, pop operators from the stack until we meet the
+        -- opening bracket. Then, pop the opening bracket from the stack
+        -- *without* pushing it into the queue.
         go stack result (RightBracket:xs) =
-            -- TODO: Handle cases where multiple brackets are around
-            -- a number: ((42))
             let (operators, newStack) = span (/= LeftBracket) stack
                 newResult  = reverse operators ++ result
             in case newStack of
                    (LeftBracket:rest) -> go rest newResult xs
                    _ -> [] -- Error
+        -- Handle binary operators. As long as there is an operator with
+        -- higher precedence on the top of the stack, push it to the
+        -- resulting queue.
+        go stack result (op1@(BinOp _):xs) =
+            let (operators, newStack) = span (hasLowerPrecedence op1) stack
+            in go (op1:newStack) (reverse operators ++ result) xs
 
-        go stack result (BinOp o1:xs) =
-            let (operators, newStack) = span (\case { (BinOp o2) -> precedence o1 <= precedence o2; _ -> False }) stack
-            in go (BinOp o1:newStack) (reverse operators ++ result) xs
 
-
-eval :: [Token] -> Maybe Integer
-eval = go []
+evalRPN :: [Token] -> Maybe Integer
+evalRPN = go []
     where
         go [result] [] = Just result
         go stack (Number n:xs) = go (n:stack) xs
@@ -91,4 +101,4 @@ eval = go []
 
 
 main :: IO ()
-main = interact (show . fromJust . eval . toRPN . tokenize)
+main = interact (show . fromJust . evalRPN . toRPN . tokenize)
